2bc524302a4c7e3ea6115a10a92f4c9eea81e2de
BossShell <mbs416@msstate.edu>
2017-09-24 23:08:50 -0500

    09/24/2017

---
+++ ArduinoSerial.py
@@ -0,0 +1,84 @@
+"""
+Filename: ArduinoSerial.py
+Description: ArduinoSerial is a class that handles serial communication between
+    the raspberry pi and the arduino.
+"""
+
+import serial
+
+class ArduinoSerial():
+    def __init__(self, serialPort = '/edv/ttyACM0', baud_rate=9600, read_timeout=5):
+        self.conn = serial.Serial(serial_port, baud_rate)
+        self.conn.timeout = read_timeout
+
+    """
+    setPinMode(pinNumber, mode)
+        - sets the pin mode (mode) for a digital pin (pinNumber)
+        - I=INPUT, O=OUTPUT, P=INPUT_PULLUP
+        - message sent across serial connection resembles the following examples:
+          MI2
+          MO8
+          MP12
+    """
+    def setPinMode(self, pinNumber, mode):
+        message = ''.join('M', mode, str(pinNumber))
+        self.conn.write(message.encode())
+
+    """
+    digitalRead(pinNumber)
+        - reads a value (readValue) from a digital pin (pinNumber)
+        - message sent across serial connection resembles the following examples:
+          RD3
+          RD12
+    """
+    def digitalRead(self, pinNumber):
+        message = ''.join('R', 'D', str(pinNumber))
+        self.conn.write(message.encode())
+        readValue = self.conn.readline().decode().strip()
+        return readValue
+    
+    """
+    digitalWrite(pinNumber, writeValue)
+        - writes a value (writeValue) to a digital pin (pinNumber)
+        - message sent across serial connection resembles the following examples:
+          WD4:1
+          WD11:0
+    """
+    def digitalWrite(self, pinNumber, writeValue):
+        message = ''.join('W', 'D', pinNumber, ':', writeValue)
+        self.conn.write(message.encode())
+    
+    """
+    analogRead(pinNumber)
+        - reads a value (readValue) from an analog pin (pinNumber)
+        - D(readValue) = [0,1023]
+        - message sent across serial connection resembles the following examples:
+          RA5
+          RA0
+    """
+    def analogRead(self, pinNumber):
+        message = ''.join('R', 'A', pinNumber)
+        self.conn.write(message.encode())
+        readValue = self.conn.readline().decode().strip()
+        return readValue
+    
+    """
+    analogWrite(pinNumber, writeValue)
+        - writes a value (writeValue) to an analog pin (pinNumber)
+        - D(writeValue) = [0,1023]
+        - message sent across serial connection resembles the following examples:
+          WA2:256
+          WA5:1020
+    """
+    def analogWrite(self, pinNumber, writeValue):
+        message = ''.join('W', 'A', pinNumber, ':', writeValue)
+        self.conn.write(message.encode())
+    
+    """
+    close()
+        - closes serial connection with the arduino
+    """
+    def close(self):
+        self.conn.close()
+        print('Arduino serial connection closed.\n.')
+        
--- arduinoSketch/arduinoSketch.ino
+++ arduinoSketch/arduinoSketch.ino
@@ -1,25 +1,97 @@
-// test code for raspi<->aruino comm
+char operation;
+char mode;
+int pinNumber;
+int digitalValue;
+int analogValue;
+int writeValue;
+int pause;
 
-char dataString[50] = {0};
-int a = 0;
+/* 
+ *  Serial data is received in the following manner:
+ *     operation mode pinNumber : writeValue
+ * 
+ *  Examples:
+ *     Set Pin Mode:  MI4
+ *     Digital Read:  DR7
+ *     Digital Write: DW4:0
+ *     Analog Read:   AR4
+ *     Analog Write:  AW0:759
+ * 
+ */
 
-int ultrasonicPin = A0;
-int distance;
-
-int wait = 100; // 1000 ms
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  // Declare outputs here and inputs (if DI?)
+  Serial.setTimeout(100);
 }
 
-void loop()
-{
-  a++;
-  distance = analogRead(ultrasonicPin) / 2;
-  sprintf(dataString, "%02X", distance);
-  Serial.println(dataString);
-  //Serial.print(distance);
-  delay(wait);
+void pinModeLocal(int pinNumber, char mode) {
+  switch(mode){
+    case 'I':
+      pinMode(pinNumber, INPUT);
+      break;
+    case 'O':
+      pinMode(pinNumber, OUTPUT);
+      break;
+    case 'P':
+      pinMode(pinNumber, INPUT_PULLUP);
+      break;
+  }
+}
+
+void digitalReadLocal(int pinNumber) {
+  digitalValue = digitalRead(pinNumber);
+  Serial.println(digitalValue);
+}
+
+void digitalWriteLocal(int pinNumber, int digitalValue) {
+  digitalWrite(pinNumber, digitalValue);
+}
+
+void analogReadLocal(int pinNumber) {
+  analogValue = analogRead(pinNumber);
+  Serial.println(analogValue);
+}
+
+void analogWriteLocal(int pinNumber, int analogValue) {
+  analogWrite(pinNumber, analogWrite);
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    operation = Serial.read();
+    delay(pause);
+    mode = Serial.read();
+    pinNumber = Serial.parseInt();
+    if(Serial.read() == ':') {
+      writeValue = Serial.parseInt();
+    }
+    switch(operation) {
+      case 'R':
+        if (mode == 'D') {
+          digitalReadLocal(pinNumber);
+        } else if (mode == 'A') {
+          analogReadLocal(pinNumber);
+        } else {
+          break;
+        }
+        break;
+        
+      case 'W':
+        if (mode == 'D') {
+          digitalWriteLocal(pinNumber, writeValue);
+        } else if (mode == 'A') {
+          analogWriteLocal(pinNumber, writeValue);
+        } else {
+          break;
+        }
+        break;
+
+      case 'M':
+        pinModeLocal(pinNumber, mode);
+        break;
+
+      default:
+        break;
+    }
+  }
 }
