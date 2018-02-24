0db416738367eec8498589644af86560e1e7d120
BossShell <bossshell94@gmail.com>
2018-02-23 22:32:19 -0600

    stuff

--- SECON_Python/MainWindow.py
+++ SECON_Python/MainWindow.py
@@ -12,11 +12,40 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
 	def appinit(self):
 		thread = debugListener(self)
-		self.connect(thread, thread.signal, self.appendDebugText)
+		self.connect(thread, thread.signal, self.receiveMessage)
 		thread.start()
 
-	def appendDebugText(self, debugMessage):
-		time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-		# Attempting to use HTML formatting, not sure if it works yet
-		row = '<tr><td>' + debugMessage + '</td><td>' + time + '</td></tr>'
-		self.debugText.append(row)
+    # Receives message from arduino serial connection.
+	# U0-U3: Microswitches
+	# R0-R4: Rangefinders
+	# D: Debug text
+	def receiveMessage(self, message):
+		item, value = message.split(':')
+
+		if item[0] == 'U':
+			if item[1] == 0:
+				#self.microswitch0value.set(value)
+			if item[1] == 1:
+				#self.microswitch1value.set(value)
+			if item[1] == 2:
+				#self.microswitch2value.set(value)
+			if item[1] == 3:
+				#self.microswitch3value.set(value)
+
+		elif item[0] == 'R':
+			if item[1] == 0:
+				#self.rangefinder0value.set(value)
+			if item[1] == 1:
+				#self.rangefinder1value.set(value)
+			if item[1] == 2:
+				#self.rangefinder2value.set(value)
+			if item[1] == 3:
+				#self.rangefinder3value.set(value)
+			if item[1] == 4:
+				#self.rangefinder4value.set(value)
+
+		else:
+			time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+			# Attempting to use HTML formatting, not sure if it works yet
+			row = '<tr><td>' + debugMessage + '</td><td>' + time + '</td></tr>'
+			self.debugText.append(row)
--- SECON_Python/main.py
+++ SECON_Python/main.py
@@ -1,21 +1,15 @@
 from mainThread import *
 from pyQtThread import *
 
-    
+
 if __name__ == '__main__':
-    mainThread = MainThread()
-    mainThread.setName('mainThread')
-    mainThread.daemon = True
-    
     pyQtThread = PyQtThread()
     pyQtThread.setName('pyQtThread')
     pyQtThread.daemon = True
 
     time.sleep(2)
-    mainThread.start()
     pyQtThread.start()
 
-    mainThread.join()
     pyQtThread.join()
 
     print('\nMain terminating...')
