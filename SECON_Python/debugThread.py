efb9f98a07f6263671e9fef00e70576e665dd0cf
BossShell <bossshell94@gmail.com>
2018-02-27 10:34:37 -0600

    test

--- SECON_Python/debugThread.py
+++ SECON_Python/debugThread.py
@@ -4,15 +4,14 @@ from PyQt4 import QtCore, QtGui
 
 class debugListener(QtCore.QThread):
     def __init__(self, parent):
-        # Setup Code
         QtCore.QThread.__init__(self, parent)
         self.signal = QtCore.SIGNAL("test Signal")
-        
+
         self.conn = serial.Serial('/dev/ttyUSB0', 57600)
         self.conn.timeout = 1
         self.end = False
         time.sleep(1)
-        
+
     def run(self):
         while self.end == False:
             self.output = self.conn.readline().decode().strip()
